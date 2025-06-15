43c2c89a3f53d83d4aff0e2c70fa99db93b83d54
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-06-15 11:35:50 -0700

    -DCMAKE_PREFIX_PATH="${PREFIX}"

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,26 +2,6 @@
 set -euo pipefail
 
 ###############################################################################
-# 0. DYNAMICALLY FIND DEPENDENCY PATHS
-###############################################################################
-
-# Find the embree cmake directory
-EMBREE_CMAKE_DIRS=( "${PREFIX}"/lib/cmake/embree-*/ )
-if [ ${#EMBREE_CMAKE_DIRS[@]} -ne 1 ]; then
-  echo "Error: Found zero or more than one embree cmake directory." >&2
-  exit 1
-fi
-EMBREE_CMAKE_DIR="${EMBREE_CMAKE_DIRS[0]%/}"
-
-# Find the TBB cmake directory
-TBB_CMAKE_DIRS=( "${PREFIX}"/lib/cmake/TBB/ )
-if [ ${#TBB_CMAKE_DIRS[@]} -ne 1 ]; then
-  echo "Error: Found zero or more than one TBB cmake directory." >&2
-  exit 1
-fi
-TBB_CMAKE_DIR="${TBB_CMAKE_DIRS[0]%/}"
-
-###############################################################################
 # 1. Configure CMake
 ###############################################################################
 cmake_args=(
@@ -30,8 +10,7 @@ cmake_args=(
   -DBUILD_SHARED_LIBS=OFF
   -DCMAKE_INSTALL_PREFIX="${SRC_DIR}/pymeshlab"
   -DCMAKE_BUILD_TYPE=Release
-  -Dembree_DIR="${EMBREE_CMAKE_DIR}"
-  -DTBB_DIR="${TBB_CMAKE_DIR}"
+  -DCMAKE_PREFIX_PATH="${PREFIX}"
   -DMESHLAB_ALLOW_OPTIONAL_EXTERNAL_LIBRARIES=ON
   -DMESHLAB_ALLOW_DOWNLOAD_SOURCE_LIBE57=OFF
   -DMESHLAB_ALLOW_DOWNLOAD_SOURCE_U3D=OFF
